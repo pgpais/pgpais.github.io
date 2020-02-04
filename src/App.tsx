bdbab4fe8fe5a27ec1e356b7cc9ca69d4de29f04
Pedro Pais <pg.pais@campus.fct.unl.pt>
2020-02-04 17:59:21 +0000

    Started working on the page. Slow but steady

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Breadcrumbs, Link} from "@material-ui/core";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <div className="Header-name">
+                <p> Pedro Pais </p>
+            </div>
+            <Breadcrumbs className="Header-nav">
+                <Link>
+                  Home
+                </Link>
+                <Link>
+                  About me
+                </Link>
+                <Link>
+                  Projects
+                </Link>
+            </Breadcrumbs>
+        </header>
+
+        <h1> This page is under development. Check back later.</h1>
     </div>
   );
 }
