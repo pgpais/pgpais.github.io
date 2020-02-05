0181b9d79ef3d09b3491cf8c4456b3e91ec8ce95
Pedro Pais <pg.pais@campus.fct.unl.pt>
2020-02-05 15:30:21 +0000

    Started experimenting with react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Breadcrumbs, Link} from "@material-ui/core";
+import {Switch, Route, BrowserRouter as Router, Link as RouterLink} from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <div className="Header-name">
-                <p> Pedro Pais </p>
-            </div>
-            <Breadcrumbs className="Header-nav">
-                <Link>
-                  Home
-                </Link>
-                <Link>
-                  About me
-                </Link>
-                <Link>
-                  Projects
-                </Link>
-            </Breadcrumbs>
-        </header>
+  return <div className="App">
+          <Router>
+              <header className="App-header">
+                  <div className="Header-name">
+                      <p> Pedro Pais </p>
+                  </div>
+                  <Breadcrumbs className="Header-nav">
+                      <RouterLink to={"/"}>
+                          Home
+                      </RouterLink>
+                      <RouterLink to={"/about"}>
+                          About me
+                      </RouterLink>
+                      <RouterLink to={"/projects"}>
+                          Projects
+                      </RouterLink>
+                  </Breadcrumbs>
+              </header>
 
-        <h1> This page is under development. Check back later.</h1>
-    </div>
-  );
-}
+              <Switch>
+                  <Route path="/about">
+                      <About/>
+                  </Route>
+                  <Route path="/">
+                      <Home/>
+                  </Route>
+              </Switch>
+          </Router>
+
+      <h1> This page is under development. Check back later.</h1>
+  </div>;
+};
+
+const Home = () => <div>Home</div>
+
+const About = () => <div>About</div>
 
 export default App;
